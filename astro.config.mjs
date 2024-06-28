adc037d124b2db2a2dcfd5b8c524e51aa41bdc35
Travis <unkletas@gmail.com>
2024-06-28 08:55:23 -0500

    added leading slash to base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://travhall.github.io",
-  base: "thd",
+  base: "/thd",
 });
