a4be535eb7c8d67b62b06644b2feea4599f49dbb
Travis <unkletas@gmail.com>
2024-07-01 08:09:08 -0500

    package cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import lighthouse from "astro-lighthouse";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), lighthouse()],
   site: "https://travhall.github.io",
   base: "/thdesign",
 });
