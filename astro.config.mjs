c963f71378313ccf5108336c2009bfc7a64eba62
Travis <unkletas@gmail.com>
2024-07-02 10:59:29 -0500

    Updated Icon location

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lighthouse from "astro-lighthouse";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lighthouse(), icon()],
+  integrations: [tailwind(), icon(), lighthouse()],
   site: "https://travhall.github.io",
-  base: "/thdesign"
+  base: "/thdesign",
 });
