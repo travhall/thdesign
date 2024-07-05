ad720daa70701564ecd048a066bf0386d72c6257
Travis <unkletas@gmail.com>
2024-07-05 07:40:30 -0500

    Set up mock project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lighthouse from "astro-lighthouse";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), lighthouse()],
+  integrations: [tailwind(), icon(), lighthouse(), mdx()],
   site: "https://travhall.github.io",
   base: "/thdesign",
 });
