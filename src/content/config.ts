a2bb16fea5516968ea373dbfee88ab965edf50c3
Travis <unkletas@gmail.com>
2024-07-05 22:24:28 -0500

    Project posts refactor

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { z, defineCollection } from "astro:content";
+const projectsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+        author: z.string(),
+        image: z.object({
+            url: z.string(),
+            alt: z.string()
+        }),
+        roles: z.array(z.string()),
+        tags: z.array(z.string()),
+        tech: z.array(z.string()),
+        status: z.array(z.string()),
+        isPublic: z.boolean()
+    })
+});
+
+export const collections = {
+    projects: projectsCollection,
+};
