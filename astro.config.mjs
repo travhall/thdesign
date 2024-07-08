77dcfc4e3aa94a33796e0d2a2c4e56b5e716140f
Travis <unkletas@gmail.com>
2024-07-08 07:03:54 -0500

    Added page transition support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import lighthouse from "astro-lighthouse";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import swup from "@swup/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), lighthouse(), mdx()],
+  integrations: [tailwind(), icon(), mdx(), swup({ theme: "overlay" })],
   site: "https://travhall.github.io",
   base: "/thdesign",
 });
